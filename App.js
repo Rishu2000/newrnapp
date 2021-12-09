3c9fdef9fba9acc7d35b87a92548bd2f12f2bd7f
Sahil Deliwala <sahildeliwala93@gmail.com>
2021-12-09 18:18:18 +0530

    To start the app use "yarn react-native start", to run on android use "yarn react-native run-android".

--- App.js
+++ App.js
@@ -71,6 +71,7 @@ const App: () => Node = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
           <Section title="Step One">
+            Rishav
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
